f28220d406db068b9b2ac7aee6951dcd50a75710
yinhaoxs <Xiashuang520>
2020-09-14 17:54:36 +0800

    delect some files

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 from utils.retrieval_feature import AntiFraudFeatureDataset
 from utils.retrieval_index import EvaluteMap
 
+
 if __name__ == '__main__':
     """
     img_dir存放所有图像库的图片，然后拿test_img_dir中的图片与图像库中的图片匹配，并输出top3的图像路径； 
@@ -9,11 +10,11 @@ if __name__ == '__main__':
     input_dim = 2048
     num_hashtables = 1
     img_dir = 'ImageRetrieval/data'
-    test_img_dir = 'ImageRetrieval/data/2020-05-12/zhuben'
-    network = 'ImageRetrieval/Image-retrieval-master/weights/gl18-tl-resnet50-gem-w-83fdc30.pth'
-    out_similar_dir = 'ImageRetrieval/RepNet-MDNet-VehicleReID-master/output/similar'
-    out_similar_file_dir = 'ImageRetrieval/RepNet-MDNet-VehicleReID-master/output/similar_file'
-    all_csv_file = 'ImageRetrieval/RepNet-MDNet-VehicleReID-master/output/aaa.csv'
+    test_img_dir = './images'
+    network = './weights/gl18-tl-resnet50-gem-w-83fdc30.pth'
+    out_similar_dir = './output/similar'
+    out_similar_file_dir = './output/similar_file'
+    all_csv_file = './output/aaa.csv'
 
     feature_dict, lsh = AntiFraudFeatureDataset(img_dir, network).constructfeature(hash_size, input_dim, num_hashtables)
     test_feature_dict = AntiFraudFeatureDataset(test_img_dir, network).test_feature()
